d5a8facac3361cefea40d7235005ccd61c3e71bb
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2022-10-01 17:53:53 -0700

    Added QuickSort

---
+++ Sorting/QuickSort/quick_sort.py
@@ -0,0 +1,24 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+
+
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
+
+
+arr = [5, 3, 1, 2, 4]
+quick_sort(arr, 0, len(arr) - 1)
+print(arr)
