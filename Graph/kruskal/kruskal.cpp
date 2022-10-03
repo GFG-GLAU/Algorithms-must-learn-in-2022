0e9610c0dcf404b6bac3ba5c28a0b34dd2c20fe0
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-10-04 00:09:13 +0530

    Update kruskal.cpp

--- Graph/kruskal/kruskal.cpp
+++ Graph/kruskal/kruskal.cpp
@@ -1,30 +1,52 @@
-struct Edge {
-    int u, v, weight;
-    bool operator<(Edge const& other) {
-        return weight < other.weight;
+// BY DSU
+#include<bits/stdc++.h>
+using namespace std;
+int par(vector<int>&v,int i)
+{
+    if(v[i]==i)
+        return i;
+    return v[i]=par(v,v[i];
+}
+void make(int a,int b,vector<int>&g,vector<int>&size)
+{
+    int u =par(g,a);
+    int v =par(g,b);
+    
+    if(size(v)>size(u))
+    swap(u,v);
+    
+    g[v]=u;
+    size[u]+=size[v];
+    
+}
+int main()
+{
+    int nodes,edge;cin>>node>>edge;
+    vector<vector<int>>v;
+    vector<int>size;
+    vector<int>g;
+    for(int i = 0;i<nodes;++i)
+    {
+        g.push_back(i);
+        size.push_back(1);
     }
-};
-
-int n;
-vector<Edge> edges;
-
-int cost = 0;
-vector<int> tree_id(n);
-vector<Edge> result;
-for (int i = 0; i < n; i++)
-    tree_id[i] = i;
-
-sort(edges.begin(), edges.end());
-
-for (Edge e : edges) {
-    if (tree_id[e.u] != tree_id[e.v]) {
-        cost += e.weight;
-        result.push_back(e);
-
-        int old_id = tree_id[e.u], new_id = tree_id[e.v];
-        for (int i = 0; i < n; i++) {
-            if (tree_id[i] == old_id)
-                tree_id[i] = new_id;
-        }
+    while(edge--)
+    {
+        int src,to,dis;cin>>src>>to>>dis;
+        v.push_back({dis,src,to});
     }
+    sort(v.begin(),v.end());
+    int sum = 0;
+    for(auto c:v)
+    {
+    int u =par(a,c[1]);
+    int v = par(b,c[2]);
+        if(u==v)
+            continue;
+        make(u,v);
+        sum+=c[0];
+    
+    }
+    cout<<sum<<"\n";
+    return  0;
 }
