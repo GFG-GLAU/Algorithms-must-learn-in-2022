43c69434dcfa29da961174fcfbbdc5b602d2a98e
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2022-10-12 02:26:49 +0530

    Update boyermore.java

--- Array/boyer more algorithm/boyermore.java
+++ Array/boyer more algorithm/boyermore.java
@@ -1,14 +1,12 @@
 import java.io.*;
  
-class GFG
+class Main
 {
  
-  // Function to find majority element
   public static int findMajority(int[] nums)
   {
     int count = 0, candidate = -1;
  
-    // Finding majority candidate
     for (int index = 0; index < nums.length; index++) {
       if (count == 0) {
         candidate = nums[index];
@@ -22,8 +20,6 @@ class GFG
       }
     }
  
-    // Checking if majority candidate occurs more than
-    // n/2 times
     count = 0;
     for (int index = 0; index < nums.length; index++) {
       if (nums[index] == candidate)
@@ -33,13 +29,8 @@ class GFG
       return candidate;
     return -1;
  
-    // The last for loop and the if statement step can
-    // be skip if a majority element is confirmed to
-    // be present in an array just return candidate
-    // in that case
   }
  
-  // Driver code
   public static void main(String[] args)
   {
     int arr[] = { 1, 1, 1, 1, 2, 3, 4 };
