23fc1a537318b64dfd13a2a87cc637e3e8c141de
Jatin Sharma <87287391+MrtitaniumJ@users.noreply.github.com>
2022-10-13 08:02:33 +0530

    Update greedy.java

--- Array/Greedy Algorithm/greedy.java
+++ Array/Greedy Algorithm/greedy.java
@@ -1,14 +1,12 @@
-// Java code for the above approach
- 
 import java.util.*;
  
 class Job {
    
-    // Each job has a unique-id,profit and deadline
+    
     char id;
     int deadline, profit;
  
-    // Constructors
+    
     public Job() {}
  
     public Job(char id, int deadline, int profit)
@@ -18,32 +16,29 @@ class Job {
         this.profit = profit;
     }
  
-    // Function to schedule the jobs take 2 arguments
-    // arraylist and no of jobs to schedule
+    
     void printJobScheduling(ArrayList<Job> arr, int t)
     {
-        // Length of array
+        
         int n = arr.size();
        
-        // Sort all jobs according to decreasing order of
-        // profit
+        
         Collections.sort(arr,
                          (a, b) -> b.profit - a.profit);
  
-        // To keep track of free time slots
+        
         boolean result[] = new boolean[t];
  
-        // To store result (Sequence of jobs)
+        
         char job[] = new char[t];
  
-        // Iterate through all given jobs
+        
         for (int i = 0; i < n; i++) {
-            // Find a free slot for this job (Note that we
-            // start from the last possible slot)
+            
             for (int j
                  = Math.min(t - 1, arr.get(i).deadline - 1);
                  j >= 0; j--) {
-                // Free slot found
+                
                 if (result[j] == false) {
                     result[j] = true;
                     job[j] = arr.get(i).id;
@@ -52,13 +47,13 @@ class Job {
             }
         }
  
-        // Print the sequence
+        
         for (char jb : job)
             System.out.print(jb + " ");
         System.out.println();
     }
  
-    // Driver's code
+    
     public static void main(String args[])
     {
         ArrayList<Job> arr = new ArrayList<Job>();
@@ -73,7 +68,7 @@ class Job {
  
         Job job = new Job();
  
-        // Function call
+        
         job.printJobScheduling(arr, 3);
     }
 }
