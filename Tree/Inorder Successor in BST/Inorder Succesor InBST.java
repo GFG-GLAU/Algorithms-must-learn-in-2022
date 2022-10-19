d8f55963db3efea9de8bf87dd7e949ef59de74c8
Jagmeet Singh <91047794+JagmeetSingh9@users.noreply.github.com>
2022-10-19 13:24:38 +0530

    Update and rename Innorder Succesor in BST to Inorder Succesor InBST.java

--- Tree/Inorder Successor in BST/Inorder Succesor InBST.java
+++ Tree/Inorder Successor in BST/Inorder Succesor InBST.java
@@ -1,6 +1,5 @@
 class Solution
 {
-    // returns the inorder successor of the Node x in BST (rooted at 'root')
   public Node inorderSuccessor(Node root,Node x)
          {
           //add code here.
